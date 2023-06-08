79ee17c4f038aac9c0da5c529170f63ddf7147ee
Chris Nguyen <chrisnguyen@Chriss-MBP.lan>
2023-06-08 15:58:08 -0700

    changes

---
+++ .next/static/webpack/app/layout.3df5c3c7ddf64a85.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/layout",{
+
+/***/ "(app-client)/./public/images/uxs-logo.svg":
+/*!************************************!*\
+  !*** ./public/images/uxs-logo.svg ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/uxs-logo.1408748a.svg\",\"height\":500,\"width\":500,\"blurWidth\":0,\"blurHeight\":0});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vcHVibGljL2ltYWdlcy91eHMtbG9nby5zdmcuanMiLCJtYXBwaW5ncyI6IjtBQUFBLCtEQUFlLENBQUMsd0dBQXdHIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8uL3B1YmxpYy9pbWFnZXMvdXhzLWxvZ28uc3ZnP2MyYjgiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1wic3JjXCI6XCIvX25leHQvc3RhdGljL21lZGlhL3V4cy1sb2dvLjE0MDg3NDhhLnN2Z1wiLFwiaGVpZ2h0XCI6NTAwLFwid2lkdGhcIjo1MDAsXCJibHVyV2lkdGhcIjowLFwiYmx1ckhlaWdodFwiOjB9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-client)/./public/images/uxs-logo.svg\n"));
+
+/***/ })
+
+});
