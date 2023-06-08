1050f68f0155359ba2e7861e89b5967400806989
Chris Nguyen <chrisnguyen@Chriss-MBP.lan>
2023-06-08 14:06:52 -0700

    new

---
+++ components/testimonials.tsx
@@ -0,0 +1,101 @@
+import Image from "next/image";
+
+import TestimonialImage01 from "@/public/images/testimonial-01.jpg";
+import TestimonialImage02 from "@/public/images/testimonial-02.jpg";
+import TestimonialImage03 from "@/public/images/testimonial-03.jpg";
+
+export default function Testimonials() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">Membership rates</h2>
+            <p className="text-xl text-gray-400">
+              Choose a plan that's right for you.
+            </p>
+          </div>
+
+          {/* Testimonials */}
+          <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
+            {/* 1st testimonial */}
+            <div
+              className="flex flex-col h-full p-6 bg-gray-800"
+              data-aos="fade-up"
+            >
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <strong>Basic Plan</strong>
+                </div>
+              </div>
+              <blockquote className="text-lg text-gray-400 grow">
+                One request at a time until you're satisfied and on to the next task. Easy credit card payment with no long term commitment and pause anytime.
+              </blockquote>
+              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                <cite className="text-gray-200 not-italic">UX</cite> -{" "}
+                <a
+                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  $4,000/month
+                </a>
+              </div>
+            </div>
+
+            {/* 2nd testimonial */}
+            <div
+              className="flex flex-col h-full p-6 bg-gray-800"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                <strong>Premium Plan</strong>
+                </div>
+              </div>
+              <blockquote className="text-lg text-gray-400 grow">
+                Our Premium package offers ongoing designs and custom coded site enhanced to your desired vision. You can pause and continue where you left off anytime.
+              </blockquote>
+              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                <cite className="text-gray-200 not-italic">UX & Dev</cite> -{" "}
+                <a
+                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  $5,000/month
+                </a>
+              </div>
+            </div>
+
+            {/* 3rd testimonial */}
+            <div
+              className="flex flex-col h-full p-6 bg-gray-800"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                <strong>Book a call</strong>
+                </div>
+              </div>
+              <blockquote className="text-lg text-gray-400 grow">
+                Request a 15 minute Zoom meeting if you would like more details on the simple and straight-forward onboarding process. 
+              </blockquote>
+              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                <cite className="text-gray-200 not-italic">Message us</cite>{" "}
+                -{" "}
+                <a
+                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  Free
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/ui/header.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import MobileMenu from './mobile-menu'
+import Image from 'next/image'
+import Logo from '@/public/images/uxs-logo.svg'
+
+export default function Header() {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Site branding */}
+          <div className="shrink-0 mr-4">
+            {/* Logo */}
+            <Link href="/" className="block" aria-label="UXSphere">
+            <Image className="max-w-full mx-auto md:max-w-none h-auto" src={Logo} width={62} height={62} alt="UXS logo" />
+            </Link>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link
+                  href="/signin"
+                  className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
+                  Sign up
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <MobileMenu />
+
+        </div>
+      </div>
+    </header>
+  )
+}
